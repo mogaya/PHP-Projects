a9e56a653c7f283a8cff30295d6d61f37b28da3d
mogaya <mogayainnocent@gmail.com>
2023-05-26 10:08:41 +0300

    4/60

--- LogIn&Register/registerform.php
+++ LogIn&Register/registerform.php
@@ -4,29 +4,33 @@
 
 if(isset($_POST['submit'])){
 
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $pass = md5($_POST['password']);
-    $cpass = md5($_POST['cpassword']);
-    $user_type = $_POST['password'];
+   $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $user_type = $_POST['user_type'];
 
-    $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
 
-    $result = mysqli_query($conn, $select);
+   $result = mysqli_query($conn, $select);
 
-    if(mysqli_num_rows($result) > 0){
-        $error[] = 'user already exists!';
-    }else{
+   if(mysqli_num_rows($result) > 0){
 
-        if($pass != $cpass){
-            $error[] = 'password not matched';
-        }else{
-            $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name','$email','$pass','$user_type')";
-            header('location:loginform.php');
-        }
-    }
+      $error[] = 'user already exist!';
+
+   }else{
+
+      if($pass != $cpass){
+         $error[] = 'password not matched!';
+      }else{
+         $insert = "INSERT INTO user_form(name, email, password, user_type) VALUES('$name','$email','$pass','$user_type')";
+         mysqli_query($conn, $insert);
+         header('location:loginform.php');
+      }
+   }
 
 };
+
 
 ?>
 
@@ -45,15 +49,14 @@ if(isset($_POST['submit'])){
 
         <form action="" method="post">
             <h3>register now</h3>
-            <?php
-            
+
+        <?php
             if(isset($error)){
                 foreach($error as $error){
                     echo '<span class="error-msg">'.$error.'</span>';
-                }
-            }
-
-            ?>
+                };
+            };
+         ?>
             <input type="text" name="name" required placeholder="enter your name">
             <input type="email" name="email" required placeholder="enter your email">
             <input type="password" name="password" required placeholder="enter your password">
